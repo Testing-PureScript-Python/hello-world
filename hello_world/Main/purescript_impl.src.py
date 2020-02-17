c43fe2089aa65d18dc155511130aefd4e48f1ce1
thautawarm <twshere@outlook.com>
2020-02-18 00:28:28 +0900

    sync compiler update

--- hello_world/Main/purescript_impl.src.py
+++ hello_world/Main/purescript_impl.src.py
@@ -1,12 +1,17 @@
 from py_sexpr.terms import *
 from py_sexpr.stack_vm.emit import module_code
+from os.path import join as joinpath
+project_path = "C:\\Users\\twshe\\Desktop\\mydb\\com-haskell\\testing\\hello-world"
 res = block( "No document"
            , assign( "$foreign"
                    , call( var('import_module')
                          , "hello_world.Main.purescript_foreign" ) )
            , assign( "ps_main"
                    , call( get_attr(var("$foreign"), "println")
-                         , metadata(6, 16, "src\Main.purs", "hello world") ) )
+                         , metadata( 6
+                                   , 16
+                                   , joinpath(project_path, "src\\Main.purs")
+                                   , "hello world" ) ) )
            , assign( "exports"
                    , record( ("main", var("ps_main"))
                            , ( "println"
--- hello_world/Test/Main/purescript_impl.src.py
+++ hello_world/Test/Main/purescript_impl.src.py
@@ -1,6 +1,12 @@
 from py_sexpr.terms import *
 from py_sexpr.stack_vm.emit import module_code
+from os.path import join as joinpath
+project_path = "C:\\Users\\twshe\\Desktop\\mydb\\com-haskell\\testing\\hello-world"
 res = block( "No document"
-           , assign("ps_main", metadata(3, 8, "test\Main.purs", 0))
+           , assign( "ps_main"
+                   , metadata( 3
+                             , 8
+                             , joinpath(project_path, "test\\Main.purs")
+                             , 0 ) )
            , assign("exports", record(("main", var("ps_main")))) )
 res = module_code(res)
